1ef47232f0e56800a5491f5c01f7e0e50bc08d1f
RaMadaSilva <raul01mateia@gmail.com>
2023-01-03 12:51:10 +0100

    teste de autenticacao

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System.Diagnostics;
+﻿using System.Net;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using AuthApi.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace AuthApi.Controllers;
 
@@ -8,6 +10,21 @@ namespace AuthApi.Controllers;
 [Route("V1")]
 public class HomeController : ControllerBase
 {
+    [HttpGet]
+    [Route("Anominous")]
+    [AllowAnonymous]
+    public string Anomino()
+        => ("Anominous");
 
+    [HttpGet]
+    [Route("Admin")]
+    [Authorize(Roles = "admin")]
+    public string AutorizadoAdmin()
+     => ("Admin autorizado");
 
+    [HttpGet]
+    [Route("gerente")]
+    [Authorize(Roles = "admin, funcionaria")]
+    public string Gerente()
+        => "Gerente";
 }
